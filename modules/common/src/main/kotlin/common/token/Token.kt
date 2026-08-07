99674c4f15af37c4a637d93432946db398f04da5
ValentinAntonioFlores <Vflores@mail.austral.edu.ar>
2026-08-06 22:30:47 -0300

    Implement tokenization logic in PrintScriptLexer and update Token data class to use SourceRange

--- modules/common/src/main/kotlin/common/token/Token.kt
+++ modules/common/src/main/kotlin/common/token/Token.kt
@@ -2,10 +2,11 @@ package common.token
 
 import com.ingsis.g14.printscript.common.token.TokenType
 import common.source.SourcePosition
+import common.source.SourceRange
 
 data class Token(
     val type: TokenType,
     val lexeme: String,
-    val position: SourcePosition,
+    val range: SourceRange,
 
     )
--- modules/lexer/src/main/kotlin/PrintScriptLexer.kt
+++ modules/lexer/src/main/kotlin/PrintScriptLexer.kt
@@ -1,11 +1,12 @@
 import com.ingsis.g14.printscript.common.token.TokenType
+import common.source.SourceRange
 import common.token.Token
 import java.io.Reader
 
 class PrintScriptLexer: Lexer {
 
 
-    private val singleCharacterToken = mapOf(
+    private val singleCharacterTokens = mapOf(
         '(' to TokenType.LEFT_PARENTHESIS,
         ')' to TokenType.RIGHT_PARENTHESIS,
         '=' to TokenType.ASSIGNMENT_OPERATOR,
@@ -18,8 +19,69 @@ class PrintScriptLexer: Lexer {
     )
 
     override fun tokenize(source: Reader): Sequence<Token> {
-        // a completar
-        return emptySequence()
+        val cursor = SourceCursor(source.readText())
+        val tokens = mutableListOf<Token>()
+
+        while(!cursor.isAtEnd()){
+            val character = cursor.peek()!!
+
+            when{
+                character.isWhitespace() -> {
+                    cursor.advance()
+                }
+
+                singleCharacterTokens.containsKey(character)-> {
+                    tokens.add(readSingleCharacterToken(cursor))
+                }
+
+                else -> {
+                    val position = cursor.currentPosition()
+
+                    throw LexicalException(
+                        message = "Unexpected character: '$character'" + "at line ${position.line}, column ${position.column}",
+                        position = position
+                    )
+                }
+            }
+        }
+        tokens.add(createEndOfFileToken(cursor))
+        return tokens.asSequence()
     }
 
+    private fun readSingleCharacterToken(
+        cursor: SourceCursor,
+    ): Token {
+        val start = cursor.currentPosition()
+        val character = cursor.advance()
+        val end = cursor.currentPosition()
+
+        val type =
+            requireNotNull(singleCharacterTokens[character]) {
+                "Character '$character' is not a registered token"
+            }
+
+        return Token(
+            type = type,
+            lexeme = character.toString(),
+            range = SourceRange(
+                start = start,
+                end = end,
+            ),
+        )
+    }
+
+    private fun createEndOfFileToken(
+        cursor: SourceCursor,
+    ): Token {
+        val position = cursor.currentPosition()
+
+        return Token(
+            type = TokenType.EOF,
+            lexeme = "",
+            range = SourceRange(
+                start = position,
+                end = position,
+            ),
+        )
+    }
 }
