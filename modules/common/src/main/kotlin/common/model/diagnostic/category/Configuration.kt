5509117657878f9fcc445a2e8d482f3de4d4a657
FelipeRawson <feliperawson111@gmail.com>
2026-08-12 20:20:54 -0300

    feat(common): added diagnostic model

---
+++ modules/common/src/main/kotlin/common/model/diagnostic/category/Configuration.kt
@@ -0,0 +1,5 @@
+package common.model.diagnostic.category
+
+data object Configuration : Category {
+    override val name: String = "Configuration"
+}
---
+++ modules/common/src/main/kotlin/common/model/diagnostic/severity/Severities.kt
@@ -0,0 +1,13 @@
+package common.model.diagnostic.severity
+
+data object Info : Severity {
+    override val name: String = "Info"
+}
+
+data object Warning : Severity {
+    override val name: String = "Warning"
+}
+
+data object Error : Severity {
+    override val name: String = "Error"
+}
