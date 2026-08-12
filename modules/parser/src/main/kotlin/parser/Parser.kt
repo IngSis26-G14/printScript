86bfe8ac0d519cf5c2e9ce37ae9759162408aa69
FelipeRawson <feliperawson111@gmail.com>
2026-08-12 20:32:36 -0300

    feat(parser): added node scanner and parser impl

---
+++ modules/parser/src/main/kotlin/parser/Parser.kt
@@ -0,0 +1,13 @@
+package parser
+
+import common.model.diagnostic.Diagnostic
+import common.model.node.Node
+import common.model.token.Token
+import common.type.outcome.Outcome
+
+interface Parser {
+    fun parse(
+        version: String,
+        tokens: Sequence<Token>,
+    ): Sequence<Outcome<Node, Diagnostic>>
+}
