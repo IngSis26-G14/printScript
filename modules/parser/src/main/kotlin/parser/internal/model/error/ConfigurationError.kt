62391735d8920a76eacb4bea7872b97152be2f1b
FelipeRawson <feliperawson111@gmail.com>
2026-08-12 20:28:43 -0300

    feat(parser): added config diagnostic and parser diagnostic

---
+++ modules/parser/src/main/kotlin/parser/internal/model/error/ConfigurationError.kt
@@ -0,0 +1,13 @@
+package parser.internal.model.error
+
+import common.model.diagnostic.Diagnostic
+import common.model.diagnostic.category.Category
+import common.model.diagnostic.category.Configuration
+import common.model.diagnostic.severity.Error
+import common.model.diagnostic.severity.Severity
+
+internal class ConfigurationError(
+    override val message: String,
+    override val severity: Severity = Error,
+    override val category: Category = Configuration,
+) : Diagnostic
