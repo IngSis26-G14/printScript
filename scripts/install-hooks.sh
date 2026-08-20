bb4bc7f543b7ab0cde04f9577e38ddd54296948e
FelipeRawson <feliperawson111@gmail.com>
2026-08-20 14:53:17 -0300

    Added pre-commit

---
+++ scripts/install-hooks.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+echo "Installing Git hooks..."
+
+git config core.hooksPath .githooks
+
+chmod +x .githooks/pre-commit
+
+echo "Git hooks installed successfully."
+echo "Pre-commit hook: .githooks/pre-commit"
